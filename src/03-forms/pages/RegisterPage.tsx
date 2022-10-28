3c55b8bfc644888afb55f41e026e85abde36cfe0
Alejandro Medina <5almero5@gmail.com>
2022-10-27 20:00:34 -0500

    Add bind changes

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,28 +1,54 @@
 import "../styles/styles.css";
-
+import { useState, ChangeEvent } from "react";
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: "Alejandro",
+    email: "alm@gmail.com",
+    password: "12345",
+    confirmPassword: "12345",
+  });
+
+  const { name, email, password, confirmPassword } = registerData;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name, e.target.value);
+  };
+
   return (
     <div>
       <h1>RegisterPage</h1>
       <form>
         <input
           type="text"
-          name="userName"
-          id="userName"
-          placeholder="User Name"
+          name="name"
+          id="name"
+          placeholder="Name"
+          value={name}
+          onChange={onChange}
         />
-        <input type="email" name="email" id="email" placeholder="Email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
         <input
           type="password"
           name="password"
           id="password"
           placeholder="Password"
+          value={password}
+          onChange={onChange}
         />
         <input
           type="password"
           name="confirmPassword"
           id="confirmPassword"
           placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={onChange}
         />
 
         <button type="submit">Register</button>
