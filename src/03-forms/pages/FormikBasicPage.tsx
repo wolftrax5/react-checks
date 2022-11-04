c04a6ece2b8ed476f22d4a75629a73abdf71018c
Alejandro Medina <5almero5@gmail.com>
2022-11-04 10:09:01 -0600

    add Formki & manual validations

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,20 +1,78 @@
 import React from "react";
+import { FormikErrors, useFormik } from "formik";
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
 export const FormikBasicPage = () => {
+  const validate = ({
+    firstName,
+    lastName,
+    email,
+  }: FormValues): FormikErrors<FormValues> => {
+    const errors: FormikErrors<FormValues> = {};
+    if (!firstName) {
+      errors.firstName = "Required";
+    }
+    if (!lastName) {
+      errors.lastName = "Required";
+    }
+    if (!email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      errors.email = "Invalid email address";
+    }
+
+    return errors;
+  };
+
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit(values) {
+      console.log("VAlues", values);
+    },
+    validate,
+  });
+
   return (
     <div>
       <h1>Formik Basic Tutorial</h1>
-      <form noValidate>
+      <form noValidate onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" id="firstName" />
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          onChange={handleChange}
+          value={values.firstName}
+        />
         <span>First Name is Required</span>
 
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" />
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          onChange={handleChange}
+          value={values.lastName}
+        />
         <span>Last Name is Required</span>
 
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={handleChange}
+          value={values.email}
+        />
         <span> Email is Required</span>
 
         <button type="submit">Submit</button>
